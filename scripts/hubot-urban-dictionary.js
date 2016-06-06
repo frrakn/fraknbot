df6e292a5de7dc9e5ccf47a3db0da3b78ff92c72
Frank Chen <cheng.frank.chen@gmail.com>
2016-06-06 02:26:52 -0400

    hubot-urban-dictionary: separated out responses to different commands

--- scripts/hubot-urban-dictionary.js
+++ scripts/hubot-urban-dictionary.js
@@ -2,31 +2,76 @@
 //   Hits UrbanDictionary API for slang definitions
 //
 // Commands:
-//   hubot what is <query> - looks up <query> on urbandictionary
+//   hubot what is <query> - returns urbandictionary definition on <query>
+//   hubot use <query> in a sentence - returns urbandictionary's example on <query>
+//   hubot urban me <query> - returns urbandictionary link to <query>
 //
 
 module.exports = function(robot) {
     var urbanDictionaryAddress = 'http://api.urbandictionary.com/v0/define?term=';
 
     robot.respond(/what is (.*)/i, function(response) {
-        var word = encodeURIComponent(response.match[1]);
+        getEntry(response.match[1], function(entry) {
+            if (entry) {
+                var def = entry['definition'];
+                if (def) {
+                    response.send(def)
+                } else {
+                    fail(response)
+                }
+            } else {
+                fail(response)
+            }
+        });
+    });
+
+    robot.respond(/use (.*) in a sentence/i, function(response) {
+        getEntry(response.match[1], function(entry) {
+            if (entry) {
+                var ex = entry['example'];
+                if (ex) {
+                    response.send(ex)
+                } else {
+                    fail(response)
+                }
+            } else {
+                fail(response)
+            }
+        });
+    });
+
+    robot.respond(/urban me (.*)/i, function(response) {
+        getEntry(response.match[1], function(entry) {
+            if (entry) {
+                var urbanLink = entry['permalink'];
+                if (urbanLink) {
+                    response.send(urbanLink)
+                } else {
+                    fail(response)
+                }
+            } else {
+                fail(response)
+            }
+        });
+    });
+
+    function getEntry(query, callback) {
+        var word = encodeURIComponent(query);
         robot.http(urbanDictionaryAddress + word)
             .header('Accept', 'application/json')
             .get()(function(err, res, body) {
                 if (err) {
-                    fail(response);
+                    callback(null);
                 }
                 var data = JSON.parse(body);
                 if (data && data['list'] && data['list'].length > 0) {
                     var entry = data['list'][0];
-                    response.send('*Definition:* ' + entry['definition']);
-                    response.send('*Example:* ' + entry['example']);
-                    response.send(entry['permalink']);
+                    callback(entry);
                 } else {
-                    fail(response);
+                    callback(null);
                 }
             });
-    });
+    }
 
     function fail(response) {
         response.send('Can\'t find it :(');
