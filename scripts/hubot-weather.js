4a3d592467ad8e7389697d4c89a6b3c90b317d0f
Frank Chen <cheng.frank.chen@gmail.com>
2016-07-21 02:15:34 -0400

    hubot-weather: added script

---
+++ scripts/hubot-weather.js
@@ -0,0 +1,22 @@
+module.exports = function(robot) {
+  var API_KEY = '235b55082e029b34';
+  robot.respond(/get weather for (.*), (.*)/, function(response) {
+    if (response.match && response.match.length == 3) {
+      var city = response.match[1];
+      var state = response.match[2];
+      robot.http('http://api.wunderground.com')
+        .header('accept', 'application/json')
+        .path('api/' + API_KEY + '/conditions/q/' + state + '/' + city + '.json')
+        .get()(function(err, res, body) {
+          var weather = JSON.parse(body);
+          if (err || !weather) {
+            response.send('Can\'t find weather info for ' + city + ', ' + state + ' :(');
+          }
+          var temperatureString = weather.current_observation.temperature_string;
+          var weatherString = weather.current_observation.weather;
+          var niceLocation = weather.current_observation.display_location.full;
+          response.send('It is ' + temperatureString + ' and ' + weatherString + ' in ' + niceLocation);
+        });
+    }
+  });
+};
